dc37840658255a880c1899df94dbd17eb2fa2f07
lcarrafabr <lcarrafa.br@gmail.com>
2020-10-27 20:46:02 -0300

    Criando Exception handler e melhorando as validações

---
+++ src/main/java/com/carrafasoft/syscondosind/api/event/listener/RecursoCriadolistener.java
@@ -0,0 +1,31 @@
+package com.carrafasoft.syscondosind.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.carrafasoft.syscondosind.api.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadolistener implements ApplicationListener<RecursoCriadoEvent>{
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		Long codigo = recursoCriadoEvent.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+		
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
--- src/main/java/com/carrafasoft/syscondosind/api/resource/CondominioResource.java
+++ src/main/java/com/carrafasoft/syscondosind/api/resource/CondominioResource.java
@@ -1,14 +1,25 @@
 package com.carrafasoft.syscondosind.api.resource;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carrafasoft.syscondosind.api.event.RecursoCriadoEvent;
 import com.carrafasoft.syscondosind.api.model.Condominios;
 import com.carrafasoft.syscondosind.api.repository.CondominioRepository;
 
@@ -19,6 +30,9 @@ public class CondominioResource {
 	@Autowired
 	private CondominioRepository condominioRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Condominios> listar() {
 		
@@ -26,10 +40,28 @@ public class CondominioResource {
 	}
 	
 	@PostMapping
-	public void criarCondominio(@RequestBody Condominios condominios) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Condominios> criarCondominio(@Valid @RequestBody Condominios condominios, HttpServletResponse response) {
 		
-		Condominios condominioSalvo = condominioRepository.save(condominios);
+		Condominios condominioSalvo =  condominioRepository.save(condominios);
 		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, condominioSalvo.getCondominioId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(condominioSalvo);
+		
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Condominios> buscarPeloId(@PathVariable Long codigo) {
+		
+		return condominioRepository.findById(codigo);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removerCondominio(@PathVariable Long codigo) {
+		
+		condominioRepository.deleteById(codigo);
 	}
 
 }
